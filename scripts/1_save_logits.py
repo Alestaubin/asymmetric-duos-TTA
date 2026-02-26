fixing the temp scaling for TENT, added Resnet18

scripts/1_save_logits.py
@@ -10,30 +10,31 @@ from src.models.inference import get_model_logits_imagenetc
 from src.utils.load_utils import load_config
 import logging
 from dotmap import DotMap 
+from src.calibration.temperature import TemperatureWrapper
 
 '''
 PHASE 1: LOGIT EXTRACTION
-This script populates the cache with logits for:
-1. f_large (Static)
-2. f_small (Static)
-3. f_large + TENT (Adaptive)
+This script populates the cache with logits for
 '''
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 parser = argparse.ArgumentParser(description='Asymmetric Duo Phase 1: Logit Extraction')
 parser.add_argument('--config', type=str, default='cfgs/save_logits.yaml', help='Path to config file')
+parser.add_argument('--debug', action='store_true', help='Enable debug mode with fewer samples')
+parser.add_argument('--model', type=str, default='resnet18', help='the model to use')
+parser.add_argument('--tent', action='store_true', default=False, help='Enable tent adaptation for the specified model')
+parser.add_argument('--ts', type=str, default=None, help='Type of temperature scaling to apply (if --tent is enabled). Options: "pts", "naive", or None (no scaling).')
 cmd_args = parser.parse_args()
 
 # Load the YAML configuration
 cfg = load_config(cmd_args.config)
 cfg = DotMap(cfg)
-# ---------------------------------------------------------
-# STEP 1: STATIC LOGIT EXTRACTION (f_large, f_small)
-# ---------------------------------------------------------
-print("\n--- Step 1: Extracting Static Logits (Source & ImageNet-C) ---")
 
-for model_name in [cfg.large_model, cfg.small_model]:
+if not cmd_args.tent:
+    print("\n--- Extracting Static Logits (Source & ImageNet-C) ---")
+    model_name = cmd_args.model
+
     print(f"\nProcessing Backbone: {model_name}")
     
     # Extract Clean Validation Logits (for calibration later)
@@ -46,31 +47,27 @@ for model_name in [cfg.large_model, cfg.small_model]:
             print(f"  Extracting: {d_name} | Severity {sev}...", end="\r")
             # The @pickle_cache inside this function handles the saving logic
             _ = get_model_logits_imagenetc(model_name, d_name, sev, cfg.data_path, batch_size=cfg.batch_size, num_workers=cfg.workers, split="test")
-print("\nStatic logit extraction complete.")
+    print("\nStatic logit extraction complete.")
+elif cmd_args.tent:
+    print("\n---  Extracting Adaptive Logits (TENT) ---")
+    print(f"\nProcessing Backbone: {cmd_args.model}")
 
-# ---------------------------------------------------------
-# STEP 2: ADAPTIVE LOGIT EXTRACTION (TENT f_large)
-# ---------------------------------------------------------
-print("\n--- Step 2: Extracting Adaptive Logits (TENT) ---")
+    tent_cfg = load_config("cfgs/tent.yaml")
+    tent_cfg = DotMap(tent_cfg)
 
-tent_cfg = load_config("cfgs/tent.yaml")
-tent_cfg = DotMap(tent_cfg)
+    target_severities = cfg.corruption.severity  # e.g., [1, 2, 3, 4, 5]
 
-target_severities = cfg.corruption.severity  # e.g., [1, 2, 3, 4, 5]
-
-for d_name in cfg.corruption.distortions:
-    print(f"\nProcessing Distortion: {d_name}")
-    timer_start = time()
-    
-    logits_dict, labels_dict = get_tent_logits_imagenet_c(
-        model_name=cfg.large_model, 
-        distortion_name=d_name, 
-        severities=target_severities, 
-        data_path=cfg.data_path, 
-        tent_cfg=tent_cfg
-    )
-    
-    print(f"Trajectory for {d_name} (Severities {target_severities}) obtained in {time() - timer_start:.2f}s")
-
-print("\n--- Phase 1 Logit Extraction Complete ---")
-print(f"All logits are cached in the 'pickles/logits_cache' directory.")
+    for d_name in cfg.corruption.distortions:
+        print(f"\nProcessing Distortion: {d_name}")
+        timer_start = time()
+        
+        logits_dict, labels_dict = get_tent_logits_imagenet_c(
+            model_name=cmd_args.model, 
+            distortion_name=d_name, 
+            severities=target_severities, 
+            data_path=cfg.data_path, 
+            tent_cfg=tent_cfg,
+            ts=cmd_args.ts
+        )
+        
+        print(f"Trajectory for {d_name} (Severities {target_severities}) obtained in {time() - timer_start:.2f}s")

scripts/2_metrics.py
@@ -45,6 +45,7 @@ def main():
     config = load_config(cmd_args.config)
     large_name = config['large_model']
     small_name = config['small_model']
+    log_event(f"Loaded Config: Large Model = {large_name}, Small Model = {small_name}")
     distortions = config['corruption']['distortions']
     severities = config['corruption']['severity']
 
@@ -57,10 +58,13 @@ def main():
 
     # Get Temperatures
     # Scalar (Independent)
-    t_large_fixed = calibrate_temperature(zl_val, labels_val, device)
-    t_small_fixed = calibrate_temperature(zs_val, labels_val, device)
+    # t_large_fixed = calibrate_temperature(zl_val, labels_val, device)
+    # t_small_fixed = calibrate_temperature(zs_val, labels_val, device)
+    t_large_fixed, t_small_fixed = 0.9101, 1.3456
     # Joint (Duo)
-    Tl_joint, Ts_joint = jointly_calibrate_temperature(zl_val, zs_val, labels_val)
+    #Tl_joint, Ts_joint = jointly_calibrate_temperature(zl_val, zs_val, labels_val)
+    Tl_joint, Ts_joint = 0.6141, 1.7131
+
 
     all_results = []
     severities = config['corruption']['severity']
@@ -107,8 +111,14 @@ def main():
                     'nll': nll,
                     'ece': ece
                 })
-
+            # log results 
             log_event(f"Processed {d_name} Sev {sev}")
+            log_event(f"Variant Metrics:")
+            for name, logits in variants.items():
+                acc = (logits.max(1)[1] == labels).float().mean().item()
+                nll = F.cross_entropy(logits, labels).item()
+                ece = metrics.ece(logits, labels)
+                log_event(f"  {name}: Acc={acc:.4f}, NLL={nll:.4f}, ECE={ece:.4f}")
 
     # 4. Save and Summarize
     df = pd.DataFrame(all_results)

src/tta/tent_utils.py
@@ -6,6 +6,8 @@ import torchvision.transforms as trn
 import torchvision.datasets as dset
 import os
 import torch.nn as nn
+from src.models.inference import get_model_logits_imagenetc
+
 
 def setup_tent(model, cfg):
     """Set up tent adaptation.
@@ -92,7 +94,8 @@ def setup_optimizer(params, cfg):
         raise NotImplementedError
 
 @pickle_cache("tent_logits_trajectory_cache")
-def get_tent_logits_imagenet_c(model_name, distortion_name, severities, data_path, tent_cfg):
+def get_tent_logits_imagenet_c(model_name, distortion_name, severities, data_path, tent_cfg, ts=None):
+    print(f"get_tent_logits_imagenet_c called with: model={model_name}, distortion={distortion_name}, severities={severities}, data_path={data_path}, tent_cfg={tent_cfg}")
     """
     Caches the adaptation trajectory for a SPECIFIC list of severities.
     Preserves model weights across the sequence for continual adaptation.
@@ -100,9 +103,22 @@ def get_tent_logits_imagenet_c(model_name, distortion_name, severities, data_pat
     from src.models.model_loader import get_model
     import torchvision.datasets as dset
     import torchvision.transforms as trn
-    
+    assert ts in [None, "pts", 'naive']
+
     # Setup Model (Loaded once per distortion trajectory)
     tented_model = get_model(model_name, freeze = False, tent_enabled=True, cfg=tent_cfg)
+    
+    if ts == "pts":
+        raise NotImplementedError("PTS temperature scaling not yet implemented for TENT. Please set ts=None or ts='naive'.")
+    elif ts == "naive":
+        print("Applying Naive Temperature Scaling to TENT model...")
+        # get the temperature using the clean validation set
+        from src.calibration.temperature import calibrate_temperature
+        # Load clean validation logits for this model
+        zl_val, labels_val = get_model_logits_imagenetc(model_name, "none", 0, tent_cfg.VAL_PATH, batch_size=tent_cfg.TEST.BATCH_SIZE, num_workers=tent_cfg.TEST.WORKERS, split="val")
+        optimal_T = calibrate_temperature(zl_val, labels_val, device=tented_model.device)
+        tented_model = TemperatureWrapper(tented_model, initial_T=optimal_T)
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     tented_model = tented_model.to(device)
     tented_model.eval()
@@ -111,8 +127,8 @@ def get_tent_logits_imagenet_c(model_name, distortion_name, severities, data_pat
     trajectory_labels = {}
 
     preprocess = trn.Compose([
-        trn.CenterCrop(224), 
-        trn.ToTensor(), 
+        trn.CenterCrop(224),
+        trn.ToTensor(),
         trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 